f40242feb9c1267994d42d641df5b4e4b325e6b7
natthasit-sriamonrat <60051605+natthasit-sriamonrat@users.noreply.github.com>
2021-05-05 05:32:53 +0700

    Delete callScript.py

--- callScript.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-os.system('python generateBidiDistricts.py kku_maps.net.xml')
-os.system('od2trips -c od2trips.config.xml -n kku_maps.net.xml.taz.xml -d OD_file_mon7.od -o od_file.odtrips.xml')
-os.system('duarouter -c duarcfg_file.trips2routers.duarcfg -o od_route_file.odtrips.rou.xml --routing-algorithm dijkstra')
